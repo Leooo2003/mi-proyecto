ee0928e2b2ae12f19502a21f92d6217f608555cd
leonardo <leosignes666@gmail.com>
2025-07-30 19:47:05 -0300

    trabajo

---
+++ trabajo1.js
@@ -0,0 +1,13 @@
+const nota= 22 
+ console.log(nota > 22);
+ if (nota > 22) {
+    console.log("nota mayor a 22");
+ }
+else if (nota < 22){
+    console.log("nota menor a 22");
+}
+else if (nota = 22) {
+    console.log("nota igual a 22");
+}
+else (nota <= 22) {
+}
